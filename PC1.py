a2b29724bdb1971fbade30ecc1759a0d21252632
Walter Rios Nima <walter.rios.n@uni.pe>
2022-06-21 03:32:32 -0500

    update

---
+++ PC1.py
@@ -0,0 +1,202 @@
+#%% ALUMNO: Rios Nima, Walter
+
+#%% PC1
+#%% Problema 1:
+
+nombre = input("Ingrese su nombre: ")
+print("¡Hola %s!" %(nombre))
+
+
+
+#%% Problema 2:
+    
+consumo=float(input("¿Cuánto fue su consumo? Ingrese su consumo: "))
+porcentajePropina=float(input("¿Cuanto de propina en porcentaje (%) quiere dar como propina?\nIngrese el porcentaje: "))
+propina=consumo*porcentajePropina/100
+print("Su propina será: %.2f" %(propina))
+
+
+
+#%% Problema 3:
+    
+payasos=int(input("Ingrese el número de payasos: "))
+muñecas=int(input("Ingrese el número de muñecas: "))
+pesoPaquete=payasos*112+muñecas*75
+print("El peso total del paquete es de %i gramos" %(pesoPaquete))
+
+
+
+#%% Problema 4:
+    
+N=int(input("Ingrese el valor de N: "))
+sumaN=N*(N+1)/2
+print("El valor de la suma de todos los enteros desde 1 hasta %i es: %i " %(N,sumaN))
+
+
+
+#%% Problema 5:
+    
+shows=int(input("Ingrese el número de shows: "))
+if shows>3:
+    k=True
+else:
+    k=False
+print(k)
+
+
+
+#%% Problema 6:
+    
+edad=int(input("Ingrese su edad: "))
+precio=0
+if edad>=18:
+    precio=10
+    print("El costo es de : %i" %(precio))
+elif edad>=4:
+    precio=5
+    print("El costo es de : %i" %(precio))
+else:
+    print("Ingresa gratis")
+
+
+
+#%% Problema 7:
+    
+numero1=float(input("Ingrese el primer número: "))
+numero2=float(input("Ingrese el segundo número: "))
+
+opcion=int(input("""\nOPCIONES:\n
+1.Mostrar la suma de los dos números.
+2.Mostrar una resta de los dos números (el primero menos el segundo).
+3.Mostrar una multiplicación de los dos números.
+\nIngrese la opción: """))
+
+if opcion==1:
+    print("La suma es: ",numero1+numero2)
+elif opcion==2:
+    print("La resta es: ",numero1-numero2)
+elif opcion==3:
+    print("La multiplicaión es: ", numero1*numero2)
+else:
+    print("Ingrese una opción válida,")
+
+
+
+#%% Problema 8:
+    
+hora=input("Ingrese la hora: ")
+verificarhora=hora.split(":")
+if len(verificarhora)==2:
+    horas, minutos = hora.split(":")
+    horas=int(horas)
+    minutos=int(minutos)
+    if horas>=18 and horas<19:
+        print("Es hora de cenar")
+    elif horas==19 and minutos==00:
+        print("Es hora de cenar")
+    elif horas>=7 and horas<8:
+        print("Es hora de desayunar")
+    elif horas==8 and minutos==00:
+        print("Es hora de desayunar")        
+    elif horas>=12 and horas<13:
+        print("Es hora de almorzar")
+    elif horas==13 and minutos==00:
+        print("Es hora de almorzar")
+    else:
+        print("")
+else:
+    print("Ingrese una hora válida.")
+
+
+
+#%% Problema 9:
+    
+lista1 =['Di', 'buen', 'día', 'a', 'papa']
+lista2=lista1.copy()
+lista2.reverse()
+print(lista1)
+print(lista2)
+
+
+
+#%% Problema 10:
+    
+listamuestra=['Rojo', 'Verde', 'Blanco', 'Negro', 'Rosa', 'Amarillo']
+listainicial=listamuestra.copy()
+posiciones= [0,4,5]
+posiciones.reverse()
+
+for i in range(len(posiciones)):
+    listamuestra.remove(listamuestra[posiciones[i]])
+print(listainicial)
+print(listamuestra)
+
+
+
+#%% Problema 11:
+    
+listaOriginal=[1, 1, 2, 3, 4, 4, 5, 1]
+listaProcesada=[]
+
+for j in listaOriginal:
+    if j not in listaProcesada:
+        listaProcesada.append(j)
+print(listaOriginal)
+print(listaProcesada)
+
+
+
+#%% Problema 12:
+
+archivo=input("Ingrese el nombre del archivo: ")
+x=archivo.split(".")
+if len(x)==2:
+    nombreArchivo, extension = archivo.split(".")
+    extension=extension.lower()
+    
+    amime={"gif":"image/gif","jpg":"image/jpeg","jpeg":"image/jpeg",
+           "png":"image/png","pdf":"application/pdf","txt":"text/plain",
+           "zip":"application/zip"}
+    
+    if extension in amime:
+       print(amime[extension])
+    else :
+        print("application/octet-stream")
+else:
+    print("Ingrese un nombre de archivo válido.")
+   
+    
+    
+    
+#%% EJERCICIOS ADICIONALES - Ejercicios diversos
+#%% EJERCICIO 1.
+
+C=float(input("Ingrese el monto a depositar: "))
+r=4
+t=3
+
+for m in range(t):
+    Cf=C*(1+r/100)**(m+1)
+    print("Ahorros tras el año %i es de %.2f" %(m+1,Cf))
+ 
+    
+ 
+#%%  EJERCICIO 2.
+
+from math import sqrt
+print("Ingrese los coeficientes de la ecuación de segundo grado (a x² + b x + c = 0)")
+a=float(input("\nIngrese el valor de a: "))
+b=float(input("Ingrese el valor de b: "))
+c=float(input("Ingrese el valor de c: "))
+
+determinante=b*b-4*a*c
+if determinante>0:
+    x1=(-b+sqrt(determinante))/(2*a)
+    x2=(-b-sqrt(determinante))/(2*a)
+    print("\nTiene dos soluciones reales, %.2f y %.2f" %(x1,x2))
+elif determinante ==0:
+    x1=-b/(2*a)
+    print("\nTiene solución unica %.2f" %(x1))
+else:
+    print("\nEcuación no presenta solución real")
+    
---
+++ PC2.py
@@ -0,0 +1,302 @@
+#%% ALUMNO: Rios Nima, Walter
+
+#%% PC2
+
+#%% PROBLEMA 1
+
+print("Los números son: ")
+for i in range (1500,2701):
+    if i%7==0 and i%5==0:
+        print (i)
+
+
+#%% PROBLEMA 2
+
+for j in range (5):
+    print("* "*(j))
+for k in range (5):
+    print("* "*(5-k))
+    
+    
+#%% PROBLEMA 3
+
+numeros=[]
+pares_numeros=[]
+impares_numeros=[]
+while True:
+    opcion=input("Quiere ingresar un número?: ")
+    if opcion.lower() =='si':
+        numero=int(input("Ingrese el número: "))
+        numeros.append(numero)
+    elif opcion.lower() == 'no':
+        break
+    else:
+        print("Entrada inválida, intente nuevamente.")
+for l in range(len(numeros)):
+    if numeros[l]%2==0:
+        pares_numeros.append(numeros[l])
+    else:
+        impares_numeros.append(numeros[l])
+        
+print("Los números ingresados son:\n",numeros)
+print("Cantidad de números pares: ",len(pares_numeros))
+print("Cantidad de números impares: ",len(impares_numeros))
+
+
+#%% PROBLEMA 4
+
+lista_sistema=[]
+n = int(input("Ingrese el número de alumnos:"))
+for m in range(n):
+    sistema = {}
+    notas=[]
+    nombre=input("Ingrese el nombre del alumno {}:  ".format(m+1))
+    for o in range(3):
+        nota = float(input("Ingrese la nota {}: ".format(o+1)))
+        notas.append(nota)
+    sistema[nombre] = notas
+    lista_sistema.append(sistema)
+print("Los alumnos y sus notas son: ")
+for p in lista_sistema:
+    print(p)
+    
+    
+#%% PROBLEMA 5
+
+def contarnumero(numero,digito):
+    numero2=[]
+    numero2=list(map(int, str(numero)))
+    return numero2.count(digito)
+
+numero = int(input("Ingrese el número:"))
+digito = int(input("Ingrese el dígito:"))
+print("Cantidad de veces {} en el número {}: {}".format(digito,numero,contarnumero(numero,digito)))
+
+
+#%% PROBLEMA 6
+
+def fibonacci():
+    fibo = [0, 1,1]
+    for r in range(2,49):
+        fibo[r] = fibo[r-1] + fibo[r-2]
+        fibo.append(fibo[r])
+    return fibo
+
+print(fibonacci())
+
+
+#%% PROBLEMA 7
+
+def primo(num):
+    if num<2:
+        return False
+    for n in range(2, num):
+        if num % n == 0:
+            return False
+        else:
+            return True
+numero_=int(input("Ingrese un número para saber si es primo: "))        
+primo(numero_)
+
+
+#%% PROBLEMA 8
+
+def factorial(num1):
+    fact=1
+    if num1==0 or num1==1:
+        return 1
+    elif num1 > 1:
+        for dd in range (1,num1+1):
+            fact=fact*dd
+        return fact
+    else:
+        print("Ingrese una opción válida")
+    
+num1=int(input("Ingrese un número: "))
+print("El factorial de {} es: {}" .format(num1,factorial(num1)))
+
+
+#%% PROBLEMA 9
+
+texto = input("Ingrese una frase o texto: ")
+texto2=''
+for kk in texto:
+    if kk.lower()=='a' or kk.lower()=="e" or kk.lower()=="i" or kk.lower()=="o" or kk.lower()=="u" : 
+        texto2 += ''
+        continue
+    texto2 += kk
+print(texto2)
+
+
+#%% PROBLEMA 10
+
+while True:
+    fecha=input("Ingrese una fecha: ")
+
+    fecha2=''
+    for k in fecha:
+        if  k=="," or k==" " or k=="/" or k=="-" or k==".": 
+            fecha2 += '.'
+            continue
+        fecha2 += k
+    fecha2=fecha2.split(".")
+
+    posicion=0
+    while posicion<len(fecha2):
+        if fecha2[posicion]=="":
+            fecha2.pop(posicion)
+        else:
+            posicion=posicion+1
+    if len(fecha2)==3:
+        break
+    else:
+        print("Entrada inválida, intente nuevamente,")
+        
+meses=[]        
+meses=["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"]
+
+mes=fecha2[0].lower()
+dia=fecha2[1]
+anio=fecha2[2]
+    
+if fecha2[0].isnumeric()==True:
+    mes=fecha2[0]    
+else:
+    for qq in range(len(meses)):
+        if mes==meses[qq]:
+            mes=qq+1
+
+print("{}-{}-{}".format( anio, str(mes).zfill(2),str(dia).zfill(2)))
+
+
+#%% PROBLEMAS ADICIONALES
+
+#%% PROBLEMA 1 - MARIO GAME - Mario.py
+
+while True:
+    altura=input("Ingrese la altura de la media pirámide: ")
+    if altura.isnumeric()==True:
+        altura=int(altura)
+        if 1<=altura<=8:
+            for j in range (altura):
+                print("  "*(altura-j)+"# "*(j+1))
+            break
+        else:
+            print("El número no está entre 1 y 8. Entrada inválida, intente nuevamente.")
+    else:
+        print("Entrada inválida, intente nuevamente.")
+        
+
+#%% PROBLEMA 2 - SUBSTITUCION - substitucion.py
+
+import string
+
+abecedario = list(string.ascii_lowercase)
+ciphertext=[]
+while True:
+    llave_sustitucion=input("Ingrese la llave de sustitucion: ")
+    if len(llave_sustitucion)==len(abecedario) and set(llave_sustitucion.lower())==set(abecedario):
+        plaintext=input("Ingrese el texto plano: ")
+        llave_sustitucion=list(llave_sustitucion)
+        plaintext=list(plaintext)
+
+        for xx in range(len(plaintext)):
+            for ll in range(len(abecedario)):
+                if plaintext[xx].islower()==True:
+                    if plaintext[xx]==abecedario[ll]:
+                        ciphertext.append(llave_sustitucion[ll].lower())
+                else:
+                    if plaintext[xx]==abecedario[ll]:
+                        ciphertext.append(llave_sustitucion[ll])
+            if plaintext[xx] not in abecedario: 
+                    ciphertext.append(plaintext[xx])
+        break
+    else:
+        print("Error")
+
+ciphertext="".join(ciphertext)
+print(ciphertext)
+
+
+#%% PROBLEMAS DIVERSOS
+
+# Nota: Estos ejercicios se realizaron de manera correlativa, es decir
+# usando las listas, variables y diccionarios de items del mismo.
+
+#%% 1.
+
+carga_alumnos=[]
+nombres=[]
+nn = int(input("Ingrese el número (n) de alumnos:"))
+
+for mm in range(nn):
+    carga = {}
+    notas_alum=[]
+    
+    nombre_alum=input("Ingrese el nombre del alumno {}: ".format(mm+1))
+    nombres.append(nombre_alum)
+    for op in range(3):
+        while True:
+            nota_alum = float(input("Ingrese la nota {}: ".format(op+1)))
+            if 0<=nota_alum<=10:
+                break
+            else:
+                print("Las notas deben estar comprendidas entre 0 y 10.")
+        notas_alum.append(nota_alum)
+        
+    
+    carga[nombre_alum] = notas_alum
+    carga_alumnos.append(carga)
+    
+print("Los alumnos y sus notas son: ")
+for p in carga_alumnos:
+    print(p)   
+
+
+#%% 2.
+
+aprobados=0
+desaprobados=0
+promedios=[]
+for ii in range(len(carga_alumnos)):
+    suma=0
+    print(carga_alumnos[ii])
+    for yy in range(3):
+        suma+=carga_alumnos[ii][nombres[ii]][yy]
+    promedio=suma/3
+    promedios.append(promedio)
+    if promedio>=4:
+        print("Aprobado, con promedio ", promedio)
+        aprobados+=1
+    else:
+        print("Desaprobado, con promedio ", promedio)
+        desaprobados+=1
+print("\nNúmero de aprobados: ", aprobados)
+print("Número de desaprobados: ", desaprobados)
+
+
+#%% 3.
+#El promedio de las tres notas de los alumnos se realizó en el item anterior.
+
+suma_aula=0
+promedio_aula=0
+for ii in range(len(carga_alumnos)):
+    suma=0
+    print(carga_alumnos[ii])
+    for yy in range(3):
+        suma+=carga_alumnos[ii][nombres[ii]][yy]
+    promedio=suma/3
+    suma_aula+=promedio
+promedio_aula=suma_aula/nn
+print("Promedio del aula: ", promedio_aula)
+
+
+#%% 4.
+
+maximo_promedio=max(promedios)
+minimo_promedio=min(promedios)
+posi_max=promedios.index(max(promedios))
+posi_min=promedios.index(min(promedios))
+print("El promedio mas alto es el de {} con {}" .format(nombres[posi_max], maximo_promedio))
+print("El promedio mas bajo es el de {} con {}" .format(nombres[posi_min], minimo_promedio))
+
